0dfd0e9850e2dade1bf39fdaf8faf69f0740b98f
CodingC1402 <CornyCodingCorn@gmail.com>
2022-05-13 17:03:38 +0700

    1

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from '@emotion/react';
-import { createTheme, CssBaseline, useMediaQuery } from '@mui/material';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './About';
-import Goals from './Goals';
-import Home from './Home';
-import Projects from './Projects';
-import Summarize from './Summarize';
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline, /**useMediaQuery*/ } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Goals from "./Goals";
+import Home from "./Home";
+import Projects from "./Projects";
+import Summarize from "./Summarize";
 
 function App() {
   //const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -18,19 +18,21 @@ function App() {
           mode: /**prefersDarkMode ? 'dark' : 'light'*/ "dark",
         },
       }),
-    [/**prefersDarkMode*/],
+    [
+      /**prefersDarkMode*/
+    ]
   );
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}>
-            <Route path="" element={<Summarize/>}/>
-            <Route path="about" element={<About/>}/>
-            <Route path="projects" element={<Projects/>}/>
-            <Route path="goals" element={<Goals/>}/>
+            <Route path="" element={<Summarize />} />
+            <Route path="about" element={<About />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="goals" element={<Goals />} />
           </Route>
         </Routes>
       </BrowserRouter>
